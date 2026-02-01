ce3344a18dc9f0105f7a9066dcf12a9e3a96b4d3
AbigailMartinezNunez01 <aby.nuz12@gmail.com>
2026-01-31 23:01:57 -0600

    Add files via upload

---
+++ VA1_1_VARIABLES/src/va1_1_variables/VA1_1_VARIABLES.java
@@ -0,0 +1,23 @@
+
+package va1_1_variables;
+
+
+public class VA1_1_VARIABLES {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //VARIABLES LOCALES:-->STACK
+        //Cualquier variable declarada en un metodo
+        int x=100;//4 bytes
+        char caracter='c';//2 bytes hasta aqui es memoria estatica
+         //VARIABLES DINAMICAS -->HEAP
+         //OBJETOS
+         Persona per =new Persona();
+        
+    }
+    
+}
+class Persona{
+    
+}
