f3ebecaf9b49f010e0de7cc9da2d9fd2321a794b
AbigailMartinezNunez01 <aby.nuz12@gmail.com>
2026-02-17 23:33:39 -0600

    Add files via upload

---
+++ EVA1_15_ARREGLOS_MULTI/src/eva1_15_arreglos_multi/EVA1_15_ARREGLOS_MULTI.java
@@ -0,0 +1,31 @@
+
+package eva1_15_arreglos_multi;
+
+
+public class EVA1_15_ARREGLOS_MULTI {
+
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int [][]matriz= new int[3][];
+        //tenemos tres filas (no hemos especificado cuantas columnas)
+        
+        matriz[0]=new int[5];// estoy creando, dentro de matriz[0] un arreglo de 5 enteros
+        matriz[1]=new int[2];
+        matriz[2]=new int[4];
+        
+        for(int i=0;i<matriz.length;i++){
+            for(int j=0; j<matriz[i].length;j++){
+                matriz[i][j]=(int)(Math.random()*100);
+            }
+        }
+        
+        for(int i=0; i < matriz.length; i++){
+            for(int j=0; j<matriz[i].length;j++){
+                System.out.print("["+ matriz[i][j]+"]");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
